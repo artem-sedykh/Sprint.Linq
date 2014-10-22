adc572c5fe3cc12b7052f640829bdb7248b9cc4a
artem-sedykh <anavrin72@gmail.com>
2014-10-22 12:48:29 +0400

    update

--- Sprint.Linq/Properties/AssemblyInfo.cs
+++ Sprint.Linq/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sprint.Linq")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Sprint.Linq is a free set of extensions for expression tree")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Sprint.Linq")]
