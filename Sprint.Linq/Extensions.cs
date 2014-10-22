36d25aa37f8747760fba134bb151d86818e10caa
artem-sedykh <anavrin72@gmail.com>
2014-10-22 11:18:53 +0400

    repo init

---
+++ Sprint.Linq/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Sprint.Linq
+{
+    public static class Extensions
+    {
+        public static LambdaExpressionDecorator<Func<T, bool>> Decorate<T>(this Expression<Func<T, bool>> expression)
+        {
+            return new LambdaExpressionDecorator<Func<T, bool>>(expression);
+        }
+
+        public static Expression<TDelegate> Expand<TDelegate>(this Expression<TDelegate> expression)
+        {
+            return expression != null ? ExpressionExpander.Expand(expression) : null;
+        }
+
+        public static Expression Expand(this Expression expression)
+        {
+            return expression != null ? ExpressionExpander.Visit(expression, null) : null;
+        }        
+    }
+}
---
+++ Sprint.Linq/LambdaExpressionDecorator.cs
@@ -0,0 +1,32 @@
+﻿namespace Sprint.Linq
+{
+    using System.Diagnostics;
+    using System.Linq.Expressions;
+
+    [DebuggerDisplay("{Expression}")]
+    public class LambdaExpressionDecorator<TDelegate>
+    {
+        private readonly Expression<TDelegate> _expression;
+        public Expression<TDelegate> Expression {
+            get { return _expression; }
+        }
+
+        public LambdaExpressionDecorator(Expression<TDelegate> expression)
+        {
+            _expression = expression;
+        }
+
+        public static implicit operator TDelegate(LambdaExpressionDecorator<TDelegate> decorator)
+        {
+
+            return (decorator != null && decorator.Expression != null)
+                ? decorator.Expression.Compile()
+                : default(TDelegate);
+        }
+
+        public static implicit operator Expression<TDelegate>(LambdaExpressionDecorator<TDelegate> decorator)
+        {
+            return decorator.Expression;
+        }
+    }
+}
