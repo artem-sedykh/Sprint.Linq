ae7084ae81f5163ede10555a1cffa4fb7748df91
anavrin72@gmail.com <anavrin72@gmail.com>
2018-08-22 11:51:51 +0300

    refactoring

--- src/Sprint.Linq.Test2/Models/Company.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sprint.Linq.Test.Models
-{
-    public class Company
-    {
-        public int Id { get; set; }
-
-        public string Name { get; set; }
-    }
-}
--- src/Sprint.Linq.Test2/Models/CompanyView.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sprint.Linq.Test.Models
-{
-    public class CompanyView
-    {
-        public int Id { get; set; }
-
-        public string Name { get; set; }
-    }
-}
